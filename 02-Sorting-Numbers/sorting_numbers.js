a3c5b1ba9f86a5d0485036bca193c312f7ac3105
Ridanna <78279799+Ridanna@users.noreply.github.com>
2021-02-18 14:50:11 +0100

    Update sorting_numbers.js

--- 02-Sorting-Numbers/sorting_numbers.js
+++ 02-Sorting-Numbers/sorting_numbers.js
@@ -1,6 +1,23 @@
 //insert your pseudocode below
 /*
-here
+var number;
+
+function listsGetSortCompare(type, direction) {
+  var compareFuncs = {
+    "NUMERIC": function(a, b) {
+        return Number(a) - Number(b); },
+    "TEXT": function(a, b) {
+        return a.toString() > b.toString() ? 1 : -1; },
+    "IGNORE_CASE": function(a, b) {
+        return a.toString().toLowerCase() > b.toString().toLowerCase() ? 1 : -1; },
+  };
+  var compare = compareFuncs[type];
+  return function(a, b) { return compare(a, b) * direction; }
+}
+
+
+number = [12, 8, 5];
+window.alert(number.slice().sort(listsGetSortCompare("NUMERIC", -1)));
 */
 
 //insert your code below
