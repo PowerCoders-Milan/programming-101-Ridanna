cca1ecc20920723e432242683c01a1dd2a4d3eb7
mahmudref@gmail.com <mahmudref@gmail.com>
2021-02-18 17:52:33 +0100

    ex 2

--- 02-Sorting-Numbers/sorting_numbers.js
+++ 02-Sorting-Numbers/sorting_numbers.js
@@ -21,3 +21,22 @@ window.alert(number.slice().sort(listsGetSortCompare("NUMERIC", -1)));
 */
 
 //insert your code below
+//insert your code below
+var number;
+
+function listsGetSortCompare(type, direction) {
+  var compareFuncs = {
+    "NUMERIC": function(a, b) {
+        return Number(a) - Number(b); },
+    "TEXT": function(a, b) {
+        return a.toString() > b.toString() ? 1 : -1; },
+    "IGNORE_CASE": function(a, b) {
+        return a.toString().toLowerCase() > b.toString().toLowerCase() ? 1 : -1; },
+  };
+  var compare = compareFuncs[type];
+  return function(a, b) { return compare(a, b) * direction; }
+}
+
+
+number = [12, 8, 5];
+window.alert(number.slice().sort(listsGetSortCompare("NUMERIC", -1)));
